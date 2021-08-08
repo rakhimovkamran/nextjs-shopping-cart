e21e19de786e5319f53e05b65f8f43e3d4518cdf
Rakhimov Kamran <rakhimovkamran@gmail.com>
2021-08-08 08:15:22 +0500

    | Fix | Font Loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,24 @@
 import type { AppProps } from "next/app"
 import "assets/styles/index.css"
+import Head from "next/head"
 
 import { Provider as StoreProvider } from "react-redux"
 import { store } from "store"
 
 function Application({ Component, pageProps }: AppProps) {
     return (
-        <StoreProvider store={store}>
-            <Component {...pageProps} />
-        </StoreProvider>
+        <>
+            <Head>
+                <meta
+                    httpEquiv="Content-Security-Policy"
+                    content="upgrade-insecure-requests"
+                />
+            </Head>
+
+            <StoreProvider store={store}>
+                <Component {...pageProps} />
+            </StoreProvider>
+        </>
     )
 }
 
